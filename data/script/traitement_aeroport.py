073820ce57810e3871f3138f1a104b06da021ec7
Olbor <32017465+OlborEgamorf@users.noreply.github.com>
2025-10-23 16:21:59 +0200

    Mise à jour des données, ajout d'une colonne "Départs"

--- data/script/traitement_aeroport.py
+++ data/script/traitement_aeroport.py
@@ -1,21 +1,36 @@
 import pandas as pd
 
-file = "data/raw/passagers_par_aeroport.csv"
+file = "data/raw/vols_par_aeroport.csv"
 df = pd.read_csv(file, delimiter=",")
 
+# df_arr = df[df["Traffic and transport measurement"] == "Passengers on board (arrivals)"]
+# df_dep = df[df["Traffic and transport measurement"] == "Passengers on board (departures)"]
+
+df_arr = df[df["Traffic and transport measurement"] == "Commercial passenger air flights (arrivals)"]
+df_dep = df[df["Traffic and transport measurement"] == "Commercial passenger air flights (departures)"]
+
 # Retrait des colonnes inutiles
-df = df.drop(columns=["STRUCTURE", "STRUCTURE_ID", "STRUCTURE_NAME", "freq", "Time frequency", "unit", "Unit of measure", "tra_meas", "Traffic and transport measurement", "schedule", "Type of schedule", "tra_cov", "Transport coverage", "CONF_STATUS", "Confidentiality status (flag)", "Time"])
+df_arr = df_arr.drop(columns=["STRUCTURE", "STRUCTURE_ID", "STRUCTURE_NAME", "freq", "Time frequency", "unit", "Unit of measure", "tra_meas", "Traffic and transport measurement", "schedule", "Type of schedule", "tra_cov", "Transport coverage", "CONF_STATUS", "Confidentiality status (flag)", "Time", "Observation status (Flag) V2 structure", "Observation value", "OBS_FLAG"])
+df_dep = df_dep.drop(columns=["STRUCTURE", "STRUCTURE_ID", "STRUCTURE_NAME", "freq", "Time frequency", "unit", "Unit of measure", "tra_meas", "Traffic and transport measurement", "schedule", "Type of schedule", "tra_cov", "Transport coverage", "CONF_STATUS", "Confidentiality status (flag)", "Time", "Observation status (Flag) V2 structure", "Observation value", "OBS_FLAG"])
 
 # Ajout d'un ID par pays
-df["COUNTRY_ID"] = df["rep_airp"].apply(lambda x: x.split("_")[0])
+df_arr["COUNTRY_ID"] = df_arr["rep_airp"].apply(lambda x: x.split("_")[0])
+df_dep["COUNTRY_ID"] = df_dep["rep_airp"].apply(lambda x: x.split("_")[0])
 
 # Rename des colonnes
-df = df.rename(columns={"rep_airp":"AIRPORT_ID", "Reporting airport":"AIRPORT_NAME", "TIME_PERIOD":"TIME", "OBS_VALUE":"VALUE", "Observation value":"OBS_VALUE", "Observation status (Flag) V2 structure":"OBS_V2"})
+df_arr = df_arr.rename(columns={"rep_airp":"AIRPORT_ID", "Reporting airport":"AIRPORT_NAME", "TIME_PERIOD":"TIME", "OBS_VALUE":"ARRIVAL_VALUE"})
+df_dep = df_dep.rename(columns={"rep_airp":"AIRPORT_ID", "Reporting airport":"AIRPORT_NAME", "TIME_PERIOD":"TIME", "OBS_VALUE":"DEPARTURE_VALUE"})
 
 # Reorder des colonnes
-cols = df.columns.tolist()
+cols = df_arr.columns.tolist()
 cols = cols[-1:] + cols[:-1]
-df = df[cols]
+df_arr = df_arr[cols]
+
+cols = df_dep.columns.tolist()
+cols = cols[-1:] + cols[:-1]
+df_dep = df_dep[cols]
+
+df = pd.merge(df_arr, df_dep, on=["AIRPORT_ID", "AIRPORT_NAME", "TIME", "COUNTRY_ID"])
 
 # Sauvegarde
-df.to_csv("passagers_par_aeroport_traite.csv", index=False)
+df.to_csv("vols_par_aeroport_traite.csv", index=False)
--- data/script/traitement_pays.py
+++ data/script/traitement_pays.py
@@ -1,13 +1,23 @@
 import pandas as pd
 
-file = "data/raw/vols_par_pays.csv"
+file = "data/raw/passagers_par_pays.csv"
 df = pd.read_csv(file, delimiter=",")
 
+# df_arr = df[df["Traffic and transport measurement"] == "Passengers on board (arrivals)"]
+# df_dep = df[df["Traffic and transport measurement"] == "Passengers on board (departures)"]
+
+df_arr = df[df["Traffic and transport measurement"] == "Commercial passenger air flights (arrivals)"]
+df_dep = df[df["Traffic and transport measurement"] == "Commercial passenger air flights (departures)"]
+
 # Retrait des colonnes inutiles
-df = df.drop(columns=["STRUCTURE", "STRUCTURE_ID", "STRUCTURE_NAME", "freq", "Time frequency", "unit", "Unit of measure", "tra_meas", "Traffic and transport measurement", "schedule", "Type of schedule", "tra_cov", "Transport coverage", "CONF_STATUS", "Confidentiality status (flag)", "Time"])
+df_arr = df_arr.drop(columns=["STRUCTURE", "STRUCTURE_ID", "STRUCTURE_NAME", "freq", "Time frequency", "unit", "Unit of measure", "tra_meas", "Traffic and transport measurement", "schedule", "Type of schedule", "tra_cov", "Transport coverage", "CONF_STATUS", "Confidentiality status (flag)", "Time", "Observation value", "Observation status (Flag) V2 structure", "OBS_FLAG"])
+df_dep = df_dep.drop(columns=["STRUCTURE", "STRUCTURE_ID", "STRUCTURE_NAME", "freq", "Time frequency", "unit", "Unit of measure", "tra_meas", "Traffic and transport measurement", "schedule", "Type of schedule", "tra_cov", "Transport coverage", "CONF_STATUS", "Confidentiality status (flag)", "Time", "Observation value", "Observation status (Flag) V2 structure", "OBS_FLAG"])
 
 # Rename des colonnes
-df = df.rename(columns={"Geopolitical entity (reporting)":"COUNTRY_NAME", "TIME_PERIOD":"TIME", "OBS_VALUE":"VALUE", "Observation value":"OBS_VALUE", "Observation status (Flag) V2 structure":"OBS_V2", "geo":"COUNTRY_ID"})
+df_arr = df_arr.rename(columns={"Geopolitical entity (reporting)":"COUNTRY_NAME", "TIME_PERIOD":"TIME", "OBS_VALUE":"ARRIVAL_VALUE", "geo":"COUNTRY_ID"})
+df_dep = df_dep.rename(columns={"Geopolitical entity (reporting)":"COUNTRY_NAME", "TIME_PERIOD":"TIME", "OBS_VALUE":"DEPARTURE_VALUE", "geo":"COUNTRY_ID"})
+
+df = pd.merge(df_arr, df_dep, on=["COUNTRY_ID","COUNTRY_NAME","TIME"])
 
 # Sauvegarde
-df.to_csv("vols_par_pays_traite.csv", index=False)
+df.to_csv("data/passagers_par_pays_traite.csv", index=False)
